1f5fd83dd8deb25c9269116e0adc24f89430c401
Екатерина Ракитова <>
2021-06-01 14:23:42 +0300

    new script

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('button');
+body = document.getElementById('body');
+
+button.addEventListener('click', function(){
+    body.classList.toggle('body-dark');
+    body.classList.remove('body');
+});
